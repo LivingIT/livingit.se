ad0686f464af6488a7cd451b3925b58ff0d88ef0
Mattias Larsson <mattias.larsson@livingit.se>
2026-01-30 19:47:26 +0100

    Text

--- src/config.ts
+++ src/config.ts
@@ -206,7 +206,7 @@ Det är **The Living IT Way**.`
         links: [],
       },
       {
-        title: 'Följ oss',
+        title: 'Följ oss här',
         social: [
           { name: 'LinkedIn', href: 'https://www.linkedin.com/company/living-it/', icon: 'linkedin' },
           { name: 'Facebook', href: 'https://www.facebook.com/LivingITConsulting', icon: 'facebook' },
